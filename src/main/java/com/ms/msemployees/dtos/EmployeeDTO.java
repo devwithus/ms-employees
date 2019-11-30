fe7fc0be2ec9d9626ddded70f3132756c256c822
devwithus <abderahim.azhrioun@gmail.com>
2019-11-30 18:18:37 +0100

    update employee DTO class

--- src/main/java/com/ms/msemployees/dtos/EmployeeDTO.java
+++ src/main/java/com/ms/msemployees/dtos/EmployeeDTO.java
@@ -1,19 +1,23 @@
 package com.ms.msemployees.dtos;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
+@Accessors(chain = true)
 @Getter
 @Setter
 @NoArgsConstructor
 public class EmployeeDTO {
-	
+
 	 @NotEmpty
 	 private String name;
 	 @NotEmpty
+	 @Email
 	 private String email;
 
 }
