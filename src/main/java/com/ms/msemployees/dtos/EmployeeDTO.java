8b5ada3fe480cfd2e69c1b564623c9da8d00b7f5
devwithus <abderahim.azhrioun@gmail.com>
2019-11-30 17:35:58 +0100

    Create Employee DTO class

---
+++ src/main/java/com/ms/msemployees/dtos/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package com.ms.msemployees.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeDTO {
+	
+	 String name;
+	 String email;
+
+}
