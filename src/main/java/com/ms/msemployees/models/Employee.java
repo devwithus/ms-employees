80cffb03da557aafd67d556a4e2916bcbbcc8400
devwithus <abderahim.azhrioun@gmail.com>
2019-11-30 17:45:52 +0100

    update Employee entity class

--- src/main/java/com/ms/msemployees/models/Employee.java
+++ src/main/java/com/ms/msemployees/models/Employee.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,7 +27,9 @@ public class Employee implements Serializable {
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private int id;
 	
-	String name;
-	String email;
+	@NotNull
+	private String name;
+	@NotNull
+	private String email;
 	
 }
