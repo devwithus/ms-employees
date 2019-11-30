d6d4e5380a23b101e5c8a89fffb382d444f6e95c
devwithus <abderahim.azhrioun@gmail.com>
2019-11-30 18:24:38 +0100

    Create Employee service First Implementation

---
+++ src/main/java/com/ms/msemployees/services/EmployeeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ms.msemployees.services;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	public EmployeeServiceImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
