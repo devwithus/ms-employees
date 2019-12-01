50b572e9517514ea9726f1cbb5e0aa5777547ed5
devwithus <abderahim.azhrioun@gmail.com>
2019-12-01 15:52:20 +0100

    Create Employee service : interface

--- src/main/java/com/ms/msemployees/services/EmployeeService.java
+++ src/main/java/com/ms/msemployees/services/EmployeeService.java
@@ -8,6 +8,9 @@ import com.ms.msemployees.models.Employee;
 public interface EmployeeService {
 	
 	List<Employee> getAllEmployees();
+	Optional<Employee> findById(int id);
 	Optional<Employee> findByName(String name);
 	Optional<Employee> findByEmail(String email);
+	Employee save(Employee emp);
+	void delete(int id);
 }
