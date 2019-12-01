4f8c1bdec8ba99d98824aa85462d18a908791d70
devwithus <abderahim.azhrioun@gmail.com>
2019-12-01 15:51:29 +0100

    Create Employee Mapper : map entity/DTO

--- src/main/java/com/ms/msemployees/mappers/EmployeeMapper.java
+++ src/main/java/com/ms/msemployees/mappers/EmployeeMapper.java
@@ -5,12 +5,16 @@ import com.ms.msemployees.models.Employee;
 
 public class EmployeeMapper {
 
-	
 	public static EmployeeDTO entityTodto(Employee emp) {
 		return new EmployeeDTO()
 					.setName(emp.getName())
 					.setEmail(emp.getEmail());
 	}
 	
+	public static Employee dtoToEntity(EmployeeDTO empdto) {
+		return new Employee()
+					.setName(empdto.getName())
+					.setEmail(empdto.getEmail());
+	}
 
 }
