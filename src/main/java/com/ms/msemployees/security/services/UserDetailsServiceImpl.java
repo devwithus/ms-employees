7a3fd5242a9fd93c29bdd228c604e880429afc1e
devwithus <abderahim.azhrioun@gmail.com>
2019-12-08 13:58:14 +0100

    Update UserDetailsServiceImpl Class

--- src/main/java/com/ms/msemployees/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ms/msemployees/security/services/UserDetailsServiceImpl.java
@@ -1,19 +1,32 @@
 package com.ms.msemployees.security.services;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.ms.msemployees.mappers.UserMapper;
+import com.ms.msemployees.models.User;
+import com.ms.msemployees.repositories.UserRepository;
+
+@Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-	public UserDetailsServiceImpl() {
-		// TODO Auto-generated constructor stub
-	}
-
+	@Autowired
+	private UserRepository userrep;
+	
 	@Override
-	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		User user = userrep.findByUsername(username)
+						   .orElseThrow(()-> new UsernameNotFoundException("User NOT Found !"));
+		
+		return UserMapper.userToPrincipal(user);
 	}
 
 }
