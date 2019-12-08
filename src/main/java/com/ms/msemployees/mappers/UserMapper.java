3e2e62766e9be48499fc1b136878d1c0720dfe72
devwithus <abderahim.azhrioun@gmail.com>
2019-12-08 13:59:05 +0100

    Class UserMapper Class

---
+++ src/main/java/com/ms/msemployees/mappers/UserMapper.java
@@ -0,0 +1,28 @@
+package com.ms.msemployees.mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.ms.msemployees.models.User;
+import com.ms.msemployees.security.beans.UserPrincipal;
+
+public class UserMapper {
+
+	public static UserPrincipal userToPrincipal(User user) {
+		
+		List<SimpleGrantedAuthority> authorities = user.getRoles()
+												.stream()
+												.map(role -> new SimpleGrantedAuthority("ROLE_"+role))
+												.collect(Collectors.toList());
+		
+		
+		return new UserPrincipal()
+					.setUsername(user.getUsername())
+					.setPassword(user.getPassword())
+					.setEnabled(user.isEnabled())
+					.setAuthorities(authorities);
+	}
+
+}
