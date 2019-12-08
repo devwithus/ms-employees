8c810b0d4d3b17a813f05d6f0f5461701f0449b0
devwithus <abderahim.azhrioun@gmail.com>
2019-12-08 15:38:14 +0100

    Update GlobalResponseException to support validation

--- src/main/java/com/ms/msemployees/exceptions/GlobalResponseException.java
+++ src/main/java/com/ms/msemployees/exceptions/GlobalResponseException.java
@@ -3,9 +3,12 @@ package com.ms.msemployees.exceptions;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,29 +17,51 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.ms.msemployees.dtos.ResponseError;
 import com.ms.msemployees.mappers.ResponseMapper;
 
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @ControllerAdvice
 public class GlobalResponseException extends ResponseEntityExceptionHandler {
 
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<String> details = new ArrayList<String>();
+		details = ex.getBindingResult()
+				    .getFieldErrors()
+					.stream()
+					.map(error -> error.getObjectName()+ " : " +error.getDefaultMessage())
+					.collect(Collectors.toList());
+		
+		ResponseError err = new ResponseError(LocalDateTime.now(), "Validation errors" ,details);
+		
+		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
+	}
+	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
        
 		List<String> details = new ArrayList<String>();
 		details.add(ex.getMessage());
-		ResponseError err = new ResponseError(LocalDateTime.now(), "NOT Found!" ,details);
+		log.info("ResourceNotFoundException : "+ex.getMessage());
+		ResponseError err = new ResponseError(LocalDateTime.now(), "Bad Request!" ,details);
 		
-		return ResponseMapper.errorToEntity(err,HttpStatus.NOT_FOUND);
+		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
 	}
 	
-	@ExceptionHandler(Exception.class)
+	
+	
+	/*@ExceptionHandler(Exception.class)
 	public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
 		
 		List<String> details = new ArrayList<String>();
-		details.add(request.getDescription(false));
-		ResponseError err = new ResponseError(LocalDateTime.now(), ex.getMessage(),details);
+		details.add(ex.getMessage());
+		ResponseError err = new ResponseError(LocalDateTime.now(), "Internal Server Error" ,details);
 		
 		return ResponseMapper.errorToEntity(err,HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+	}*/
 	
 	
 }
