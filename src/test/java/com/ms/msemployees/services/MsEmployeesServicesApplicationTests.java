9a1aed653f03bc222edd33e6ffd75d51ee7f9b52
Azhwani <58115545+devwithus@users.noreply.github.com>
2020-02-14 17:22:03 +0100

    Update MsEmployeesServicesApplicationTests.java

--- src/test/java/com/ms/msemployees/services/MsEmployeesServicesApplicationTests.java
+++ src/test/java/com/ms/msemployees/services/MsEmployeesServicesApplicationTests.java
@@ -31,38 +31,38 @@ public class MsEmployeesServicesApplicationTests {
 	private EmployeeServiceImpl empserv;
 	
 	@Test
-    public void whenFindAll()
-    {
-        List<Employee> list = new ArrayList<Employee>();
-        Employee empOne   = new Employee(1, "John", "howtodoinjava@gmail.com");
-        Employee empTwo   = new Employee(2, "Alex", "alexk@yahoo.com");
-        Employee empThree = new Employee(3, "Steve", "swaugh@gmail.com");
-         
-        list.add(empOne);
-        list.add(empTwo);
-        list.add(empThree);
-        
-        Mockito.when(emprepo.findAll()).thenReturn(list);
-         
-        //test
-        List<Employee> empList = empserv.getAllEmployees();
-         
-        assertThat(empList.size(),equalTo(3));
+    	public void whenFindAll()
+    	{
+        	List<Employee> list = new ArrayList<Employee>();
+		Employee empOne   = new Employee(1, "John", "howtodoinjava@gmail.com");
+		Employee empTwo   = new Employee(2, "Alex", "alexk@yahoo.com");
+		Employee empThree = new Employee(3, "Steve", "swaugh@gmail.com");
 
-    }
+		list.add(empOne);
+		list.add(empTwo);
+		list.add(empThree);
+
+		Mockito.when(emprepo.findAll()).thenReturn(list);
+
+		//test
+		List<Employee> empList = empserv.getAllEmployees();
+
+		assertThat(empList.size(),equalTo(3));
+
+   	 }
 	
 	@Test
-    public void whenFindById()
-    {
-        Employee empOne   = new Employee(1, "John", "howtodoinjava@gmail.com");
-        Mockito.when(emprepo.findById(1)).thenReturn(Optional.of(empOne));
+   	 public void whenFindById()
+    	{
+        	Employee empOne   = new Employee(1, "John", "howtodoinjava@gmail.com");
+       		 Mockito.when(emprepo.findById(1)).thenReturn(Optional.of(empOne));
          
-        //test
-        Optional<Employee> emp = empserv.findById(1);
+        	//test
+        	Optional<Employee> emp = empserv.findById(1);
          
-        assertThat(emp.get().getEmail(),equalTo(empOne.getEmail()));
+        	assertThat(emp.get().getEmail(),equalTo(empOne.getEmail()));
 
-    }
+    	}
 	
 	
 
