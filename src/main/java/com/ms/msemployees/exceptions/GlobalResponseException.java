3ce6a0e78b4f14a63a152fb3a709dbcbf0f34d13
ELITEONE <ELITEONE@DESKTOP-KFHBRKP>
2020-03-25 14:34:36 +0100

    update some files

--- src/main/java/com/ms/msemployees/exceptions/GlobalResponseException.java
+++ src/main/java/com/ms/msemployees/exceptions/GlobalResponseException.java
@@ -10,22 +10,62 @@ import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ms.msemployees.dtos.ResponseError;
 import com.ms.msemployees.mappers.ResponseMapper;
 
-import lombok.extern.slf4j.Slf4j;
 
 
-@Slf4j
 @ControllerAdvice
 public class GlobalResponseException extends ResponseEntityExceptionHandler {
 
+	// handleHttpMediaTypeNotSupported : triggers when the JSON is invalid
+	@Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+		
+		List<String> details = new ArrayList<String>();
+		
+		
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+        
+        details.add(builder.toString());
+
+        ResponseError err = new ResponseError(LocalDateTime.now(), "Invalid JSON" ,details);
+		
+		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
+	
+	}
+	
+	// handleHttpMessageNotReadable : triggers when the JSON is malformed
+	@Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        
+		List<String> details = new ArrayList<String>();
+        details.add(ex.getMessage());
+        ResponseError err = new ResponseError(LocalDateTime.now(), "Malformed JSON request" ,details);
+		
+		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
+    }
+	
+	// handleMethodArgumentNotValid : triggers when @Valid fails
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -42,6 +82,33 @@ public class GlobalResponseException extends ResponseEntityExceptionHandler {
 		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
 	}
 	
+	// handleMissingServletRequestParameter : triggers when there are missing parameters
+	@Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+		
+		List<String> details = new ArrayList<String>();
+		details.add(ex.getParameterName() + " parameter is missing");
+
+        ResponseError err = new ResponseError(LocalDateTime.now(), "Missing Parameters" ,details);
+		
+		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
+    }
+	
+	// handleMethodArgumentTypeMismatch : triggers when a parameter's type does not match
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+                                                                      WebRequest request) {
+        List<String> details = new ArrayList<String>();
+		details.add(ex.getMessage());
+      
+        ResponseError err = new ResponseError(LocalDateTime.now(), "Mismatch Type" ,details);
+		
+		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
+    }
+	
+	// handleConstraintViolationException : triggers when @Validated fails
 	@ExceptionHandler(ConstraintViolationException.class)
 	public ResponseEntity<?> handleConstraintViolationException(Exception ex, WebRequest request) {
 		
@@ -52,29 +119,30 @@ public class GlobalResponseException extends ResponseEntityExceptionHandler {
 		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
 	}
 	
+	// handleResourceNotFoundException : triggers when there is not resource with the specified ID in BDD
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
        
 		List<String> details = new ArrayList<String>();
 		details.add(ex.getMessage());
-		log.info("ResourceNotFoundException : "+ex.getMessage());
-		ResponseError err = new ResponseError(LocalDateTime.now(), "Resource NotFound" ,details);
+		ResponseError err = new ResponseError(LocalDateTime.now(), "Resource Not Found" ,details);
 		
 		return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
 	}
 	
-	
-	
-	
-	/*@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+	// handleNoHandlerFoundException : triggers when the handler method is invalid
+	@Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+            NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<String> details = new ArrayList<String>();
+		details.add(String.format("Could not find the %s method for URL %s", ex.getHttpMethod(), ex.getRequestURL()));
+		ResponseError err = new ResponseError(LocalDateTime.now(), "Method Not Found" ,details);
 		
-		List<String> details = new ArrayList<String>();
-		details.add(ex.getMessage());
-		ResponseError err = new ResponseError(LocalDateTime.now(), "Internal Server Error" ,details);
-		
-		return ResponseMapper.errorToEntity(err,HttpStatus.INTERNAL_SERVER_ERROR);
-	}*/
+        return ResponseMapper.errorToEntity(err,HttpStatus.BAD_REQUEST);
+        
+    }
+	
 	
 	
 }
